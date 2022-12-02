822a801d47a8b8eede4839ebf53d6a28741017ed
nvdinh185 <nvdinh0766777123@gmail.com>
2022-12-02 11:18:22 +0700

    x

--- client/js/main.js
+++ client/js/main.js
@@ -4,54 +4,10 @@ headingElement.innerText = 'Sản phẩm nổi bật';
 
 const ulElement = document.querySelector("#list");
 
-const listHoa = [
-    {
-        id: 1,
-        img: 'images/ThuyTien_01.jpg',
-        title: 'Hoa thủy tiên',
-        description: `Những bát hoa thủy tiên đẹp chưng trong nhà ngày Tết vốn là thú chơi tao nhã, 
-        thanh lịch, hào hoa của người Hà Nội xưa`
-    },
-    {
-        id: 2,
-        img: 'images/hoaviolet2.jpg',
-        title: 'Hoa Violet',
-        description: `Hoa phi yến còn có tên hoa chân chim vì hoa trông giống như chân con chim 
-        hoặc phi yến (chim yến đang bay) hay violet vì hoa màu tím và còn có tên La-let
-        hay đông thảo thuộc họ Mao lương (Ranuncolaceae) thực chất cũng có cây cho
-        hoa màu hồng và trắng song rất ít.`
-    },
-    {
-        id: 3,
-        img: 'images/hoalili.PNG',
-        title: 'Hoa Lilies',
-        description: `Những giọt nước mắt nào có giúp được gì! Giắc sẽ phải lên đường chinh chiến
-        ở một xứ xa lạ, đành bỏ lại Lilia, người vợ chưa cưới của mình trên đất Pháp.
-        Lúc chia tay, Giắc rút trái tim ra khỏi lồng ngực mình, trao cho Lilia và nói`
-    },
-    {
-        id: 4,
-        img: 'images/hoalai.jpg',
-        title: 'Hoa lài',
-        description: `Mùi hương thoảng thoảng như cứ đeo đẳng theo chúng ta mọi nơi lại phát ra từ
-        những bông hoa trắng nhỏ xíu, mong manh.`
-    },
-    {
-        id: 5,
-        img: 'images/ThuyTien_01.jpg',
-        title: 'Hoa phong lan',
-        description: `Nếu không có ảnh hậu trường, khó ai hình dung được chiêu thức chụp bức ảnh
-        hoa lan này. Nhiều dân mạng đã đặt tên cho tác phẩm này là: "Tôi đã thấy hoa
-        vàng trên quần đen". Người ta hay bảo chỉ có giới trẻ mới tiếp cận được những
-        "tinh hoa" của công nghệ chỉnh sửa ảnh lung linh ảo diệu thời hiện đại.
-        `
-    }
-];
-
 (async () => {
     let listHoa = await axiosClient.get('/hoa/listhoa');
     listHoa = listHoa.data;
-    console.log("listHoa: ", listHoa);
+    // console.log("listHoa: ", listHoa);
     listHoa.forEach(function (hoa) {
         const liElement = document.createElement('li');
 
