e4d89816e94a8f57a1a9d454132e9dc19a8e011d
nvdinh185 <nvdinh0766777123@gmail.com>
2023-04-09 15:15:58 +0700

    x

--- client/axiosClient.js
+++
@@ -1,8 +0,0 @@
-const url = "http://localhost:3000";
-
-const axiosClient = axios.create({
-    baseURL: url,
-    headers: {
-        'content-type': 'application/json',
-    },
-});
--- client/js/main.js
+++ client/js/main.js
@@ -5,48 +5,23 @@ headingElement.innerText = 'Sản phẩm nổi bật';
 const ulElement = document.querySelector("#list");
 
 (async () => {
-    let listHoa = await axios.get('/hoa/listhoa');
+    var listHoa = await axios.get('http://localhost:3000/hoa/listhoa');
     listHoa = listHoa.data;
     // console.log("listHoa: ", listHoa);
     listHoa.forEach(function (hoa) {
         const liElement = document.createElement('li');
 
-        const divLeftElement = document.createElement('div');
-        divLeftElement.className = 'left';
-        const aLeftElement = document.createElement('a');
-        aLeftElement.href = '#';
-        const imgLeftElement = document.createElement('img');
-        imgLeftElement.src = hoa.image;
-        imgLeftElement.alt = hoa.image;
-        aLeftElement.appendChild(imgLeftElement);
-        divLeftElement.appendChild(aLeftElement);
-        liElement.appendChild(divLeftElement);
-
-        const divRightElement = document.createElement('div');
-        divRightElement.className = 'right';
-
-        const h2Element = document.createElement('h2');
-        const aRightElement = document.createElement('a');
-        aRightElement.href = '#';
-        aRightElement.innerText = hoa.title;
-        h2Element.appendChild(aRightElement);
-        divRightElement.appendChild(h2Element);
-
-        const pElement = document.createElement('p');
-        pElement.innerText = hoa.description;
-        divRightElement.appendChild(pElement);
-
-        const spanElement = document.createElement('span');
-        const aSpanElement = document.createElement('a');
-        aSpanElement.href = '#';
-        aSpanElement.innerText = '-Chi tiết-';
-        spanElement.appendChild(aSpanElement);
-        divRightElement.appendChild(spanElement);
-        liElement.appendChild(divRightElement);
-
-        const divClrElement = document.createElement('div');
-        divClrElement.className = 'clr';
-        liElement.appendChild(divClrElement);
+        liElement.innerHTML = `
+            <div class="left">
+                <a href="#" title=""><img src=${hoa.image} alt=${hoa.image} /></a>
+            </div>
+            <div class="right">
+                <h2><a href="#" title="">${hoa.title}</a></h2>
+                <p>${hoa.description}</p>
+                <span><a href="#" title="">-Chi tiết-</a></span>
+            </div>
+            <div class="clr"></div>
+        `;
 
         ulElement.appendChild(liElement);
 
