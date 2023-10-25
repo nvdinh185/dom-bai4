f358251a65a153daea73f5362f433a8efbca125a
nvdinh185 <nvdinh0766777123@gmail.com>
2023-10-25 16:21:26 +0700

    x

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ var listHoa = [
     {
         id: 1,
         name: 'Hoa thủy tiên',
-        img: 'ThuyTien_01.jpg',
+        image: 'ThuyTien_01.jpg',
         description: `Những bát hoa thủy tiên đẹp chưng trong nhà ngày Tết vốn là thú chơi tao nhã, thanh lịch,
         hào hoa của người Hà Nội xưa`
     },
     {
         id: 2,
         name: 'Hoa Violet',
-        img: 'hoaviolet2.jpg',
+        image: 'hoaviolet2.jpg',
         description: `Hoa phi yến còn có tên hoa chân chim vì hoa trông giống như chân con chim hoặc phi yến
         (chim yến đang bay) hay violet vì hoa màu tím và còn có tên La-let hay đông thảo thuộc họ Mao lương
         (Ranuncolaceae) thực chất cũng có cây cho hoa màu hồng và trắng xong rất ít.`
@@ -17,7 +17,7 @@ var listHoa = [
     {
         id: 3,
         name: 'Hoa Lilies',
-        img: 'hoalili.PNG',
+        image: 'hoalili.PNG',
         description: `Những giọt nước mắt nào có giúp được gì! Giắc sẽ phải lên đường chinh chiến ở một xứ xa lạ,
         đành bỏ lại Lilia, người vợ chưa cưới của mình trên đất Pháp. Lúc chia tay, Giắc rút trái tim ra khỏi
         lồng ngực mình, trao cho Lilia và nói`
@@ -25,14 +25,14 @@ var listHoa = [
     {
         id: 4,
         name: 'Hoa lài',
-        img: 'hoalai.jpg',
+        image: 'hoalai.jpg',
         description: `Mùi hương thoảng thoảng như cứ đeo đẳng theo chúng ta mọi nơi lại phát ra từ những
         bông hoa trắng nhỏ xíu, mong manh.`
     },
     {
         id: 5,
         name: 'Hoa phong lan',
-        img: 'hoaphonglan.jpg',
+        image: 'hoaphonglan.jpg',
         description: `Nếu không có ảnh hậu trường, khó ai hình dung được chiêu thức chụp bức ảnh hoa lan này.
         Nhiều dân mạng đã đặt tên cho tác phẩm này là: "Tôi đã thấy hoa vàng trên quần đen". Người ta hay bảo chỉ
         có giới trẻ mới tiếp cận được những "tinh hoa" của công nghệ chỉnh sửa ảnh lung linh ảo diệu thời hiện đại.`
