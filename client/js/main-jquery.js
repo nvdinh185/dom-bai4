613c972f179aeb938c03d4b3d7be22dae3144221
nvdinh185 <nvdinh0766777123@gmail.com>
2023-10-25 18:55:26 +0700

    x

---
+++ client/js/main-jquery.js
@@ -0,0 +1,37 @@
+var headingElement = $("#heading");
+
+headingElement.text('Sản phẩm nổi bật');
+
+const ulElement = $("#list");
+
+(async () => {
+    try {
+        var listHoa = await axios.get('http://localhost:3000/hoa');
+        listHoa = listHoa.data;
+        // console.log("listHoa: ", listHoa);
+        listHoa.forEach(function (hoa) {
+            const liElement = $('<li>');
+
+            liElement.html(`
+                <div class="left">
+                    <a href="#" title=""><img src=images/${hoa.image} alt=${hoa.image} /></a>
+                </div>
+                <div class="right">
+                    <h2><a href="#" title="">${hoa.name}</a></h2>
+                    <p>${hoa.description}</p>
+                    <span><a href="#" title="">-Chi tiết-</a></span>
+                </div>
+                <div class="clr"></div>
+            `);
+
+            ulElement.append(liElement);
+
+        });
+    } catch (err) {
+        console.log('Lỗi ' + err);
+        const liElement = $('<li>');
+        liElement.text('Xảy ra lỗi khi lấy dữ liệu!');
+        liElement.attr('style', 'display: block; color: red; font-style: italic;');
+        ulElement.append(liElement);
+    }
+})()
