7d3f0bd6fa181ab75ad0e0322b6cfcd28fbe38e0
nvdinh185 <nvdinh0766777123@gmail.com>
2023-10-26 17:14:22 +0700

    x

--- js/main.js
+++ js/main.js
@@ -46,8 +46,9 @@ headingElement.text('Sản phẩm nổi bật');
 
 const ulElement = $("#list");
 
-var htmls = listHoa.map(function (hoa) {
-    return `<li>
+var htmls = '';
+listHoa.forEach(function (hoa) {
+    htmls += `<li>
         <div class="left">
             <a href="#" title=""><img src="images/${hoa.image}" alt="${hoa.image}" /></a>
         </div>
@@ -60,4 +61,4 @@ var htmls = listHoa.map(function (hoa) {
     </li>`;
 })
 
-ulElement.html(htmls.join(''));
+ulElement.html(htmls);
